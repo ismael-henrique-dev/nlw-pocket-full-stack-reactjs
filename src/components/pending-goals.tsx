6798650b8469866e4ea14dec708eea495d5b7ad4
Ismael <ismael.henrique.dev@gmail.com>
2024-09-20 21:18:35 -0300

    fix bugs

--- src/components/pending-goals.tsx
+++ src/components/pending-goals.tsx
@@ -7,36 +7,30 @@ import { createGoalCompletion } from '../http/create-goal-completion'
 export function PendingGoals() {
   const queryClient = useQueryClient()
 
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ['pending-goals'],
     queryFn: getPendingGoals,
-    staleTime: 60 * 1000,
   })
 
-  if (!data) {
+  if (isLoading || !data) {
     return null
   }
 
-  console.log(data)
-
-  async function handleCompletionGoal(goalId: string) {
+  async function handleCreateGoalCompletion(goalId: string) {
     await createGoalCompletion(goalId)
 
-    queryClient.invalidateQueries({ queryKey: ['summary'] })
     queryClient.invalidateQueries({ queryKey: ['pending-goals'] })
+    queryClient.invalidateQueries({ queryKey: ['summary'] })
   }
+
   return (
     <div className="flex flex-wrap gap-3">
       {data.map(goal => {
-        console.log(
-          `Aqui está o desiredWeeklyFrequency: ${goal.deseridWeeklyFrequency}`
-        )
-        console.log(goal.completionCount)
         return (
           <OutlineButton
             key={goal.id}
-            disabled={goal.deseridWeeklyFrequency <= goal.completionCount}
-            onClick={() => handleCompletionGoal(goal.id)} // Sempre que eu tiver uma função com parâmetro eu executo dessa forma
+            onClick={() => handleCreateGoalCompletion(goal.id)}
+            disabled={goal.completionCount >= goal.desiredWeeklyFrequency}
           >
             <Plus className="size-4 text-zinc-600" />
             {goal.title}
--- src/http/get-pending-goals.ts
+++ src/http/get-pending-goals.ts
@@ -1,14 +1,12 @@
 type PendingGoalsResponse = {
   id: string
   title: string
-  deseridWeeklyFrequency: number
+  desiredWeeklyFrequency: number
   completionCount: number
 }[]
 
 export async function getPendingGoals(): Promise<PendingGoalsResponse> {
   const response = await fetch('http://localhost:3333/pendding-goals')
   const data = await response.json()
-  console.log(data.penddingGoals[0].deseridWeeklyFrequency)
-  return data.penddingGoals
-
+  return data.pendingGoals
 }
